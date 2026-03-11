e47df6fb5dd7a641d4ee6fc4e8f71870276087fd
vzaqoyan128 <vzaqoyan128@gmail.com>
2026-03-11 23:56:44 +0400

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+Use master;
+GO
+IF EXISIS (SELECT 1 FROM sys.database WHERE name = 'DataWarehouse')
+BEGIN
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO 
+CREATE DATABASE DataWarehouse;
+GO 
+USE DataWarehouse;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
